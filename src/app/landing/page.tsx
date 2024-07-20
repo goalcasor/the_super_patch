90494d7fa6f62764952035b23b71166b3ec1b96d
andresc4f <164497480+goalcasor@users.noreply.github.com>
2024-07-20 21:37:17 +0200

    add new video to landing

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -4,14 +4,18 @@ const VideoSection = () => {
   return (
     <div className={styles.landing}>
         <div className={`${styles.video_section_content}`}>
+            <h1>¡Gracias por estar aquí! <br/> Antes de avanzar es importante que veas estos 2 videos.</h1>
             <div className={styles.video_container}>
+              <video controls id='video_principal'>
+                <source src="/videos/intro.mp4" type="video/mp4" />
+              </video>
               <video controls id='video_principal'>
                 <source src="/videos/funnel_video_01.mp4" type="video/mp4" />
               </video>
-              <a href="https://wa.me/message/U47Z7TQCMLBGJ1" className={styles.wp_button}>
-                Acceder a Whatsapp
-              </a>
             </div>
+            <a href="https://wa.me/message/U47Z7TQCMLBGJ1" className={styles.wp_button}>
+              Acceder a Whatsapp
+            </a>
         </div>
     </div>
   );
