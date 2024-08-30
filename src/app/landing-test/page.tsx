67151ffb4b4d301deca772422fa3a3b840e34688
andresc4f <164497480+goalcasor@users.noreply.github.com>
2024-08-30 18:47:47 +0200

    Update page.tsx

--- src/app/landing-test/page.tsx
+++ src/app/landing-test/page.tsx
@@ -1,18 +1,25 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import styles from '@/styles/pages/LandingTest.module.scss';
 
-// Aquí se importa el array de preguntas abstraído
-import { questions } from '@/data/QuestionsLandingTest'; // Asegúrate de que la ruta sea correcta
+// Importa las preguntas y la interfaz Question
+import { questions, Question } from '@/data/QuestionsLandingTest';
+
+// Interfaz para la evaluación
+interface Evaluation {
+  score: number;
+  isQualified: boolean;
+}
 
 export default function LandingTest() {
-  const [responses, setResponses] = useState([]);
-  const [evaluation, setEvaluation] = useState(null);
+  // Inicialización del estado con tipos correctos
+  const [responses, setResponses] = useState<string[]>([]);
+  const [evaluation, setEvaluation] = useState<Evaluation | null>(null);
   const swiperRef = useRef(null);
 
   const handleAnswerSelection = (index: number, answer: string): void => {
@@ -31,14 +38,13 @@ export default function LandingTest() {
   const evaluateResponses = (responses: string[]): void => {
     let score = 0;
 
-    responses.forEach((response: string, index: number) => {
-      if (response.includes('a')) score += 1; // Puedes ajustar la lógica según tus necesidades
+    responses.forEach((response: string) => {
+      if (response.includes('a')) score += 1;
       if (response.includes('b')) score += 2;
       if (response.includes('c')) score += 1;
     });
 
-    // Establecer un umbral para determinar si el usuario es un buen candidato
-    const isQualified: boolean = score >= 15; // Puedes ajustar este umbral
+    const isQualified: boolean = score >= 15;
 
     setEvaluation({
       score,
@@ -57,20 +63,20 @@ export default function LandingTest() {
         className={styles.swiper}
         onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
-        {questions.map((item, index) => (
+        {questions.map((item: Question, index: number) => (
           <SwiperSlide key={index} className={styles.swiper_slide}>
             <div className={styles.question_container}>
               <h3 className={styles.question}>{item.question}</h3>
               <div className={styles.answers_container}>
-              {item.answers.map((answer, answerIndex) => (
-                <button 
-                  key={answerIndex} 
-                  onClick={() => handleAnswerSelection(index, answer)}
-                  className={styles.answer}
-                >
-                  {answer}
-                </button>
-              ))}
+                {item.answers.map((answer, answerIndex) => (
+                  <button
+                    key={answerIndex}
+                    onClick={() => handleAnswerSelection(index, answer)}
+                    className={styles.answer}
+                  >
+                    {answer}
+                  </button>
+                ))}
               </div>
             </div>
           </SwiperSlide>
@@ -82,35 +88,30 @@ export default function LandingTest() {
               <div>
                 <div>
                   <h4>Evaluación</h4>
+                  <p>Preguntas totales: {questions.length}</p>
                   <p>Puntuación Total: {evaluation.score}</p>
+                  
                   <p>
                     {evaluation.isQualified
                       ? '¡Felicidades! Eres un buen candidato para esta oportunidad.'
                       : 'Gracias por tu tiempo. Parece que esta oportunidad podría no ser la mejor para ti.'}
                   </p>
-
                 </div>
                 <br />
                 <br />
                 <div>
-                <br />
-                <h4>Lógica de Evaluación:</h4>
-                <br />
-                <p><strong>Puntuación por Respuesta:</strong></p>
-                <br />
-
-                <ul>
-                  <li>Respuestas que contienen <strong>a</strong>: 1 punto.</li>
-                  <li>Respuestas que contienen <strong>b</strong>: 2 puntos.</li>
-                  <li>Respuestas que contienen <strong>c</strong>: 1 punto.</li>
-                </ul>
-                <br />
-                <p><strong>Umbral:</strong></p>
-                <br />
-                <ul>
-                  <li>Si la puntuación total es 15 o más, el usuario pasa el test </li>
-                  <li>Si la puntuación es menos de 15, el usuario no pasa el test </li>
-                </ul>
+                  <h4>Lógica de Evaluación:</h4>
+                  <p><strong>Puntuación por Respuesta:</strong></p>
+                  <ul>
+                    <li>Respuestas que contienen <strong>a</strong>: 1 punto.</li>
+                    <li>Respuestas que contienen <strong>b</strong>: 2 puntos.</li>
+                    <li>Respuestas que contienen <strong>c</strong>: 1 punto.</li>
+                  </ul>
+                  <p><strong>Umbral:</strong></p>
+                  <ul>
+                    <li>Si la puntuación total es 15 o más, el usuario pasa el test.</li>
+                    <li>Si la puntuación es menos de 15, el usuario no pasa el test.</li>
+                  </ul>
                 </div>
               </div>
             )}
