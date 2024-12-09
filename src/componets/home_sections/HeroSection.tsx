d18aac0a81c5a14ae2765502d8701571770c5592
Andrés <164497480+goalcasor@users.noreply.github.com>
2024-12-09 13:08:09 +0100

    Go To Other PC

--- src/componets/home_sections/HeroSection.tsx
+++ src/componets/home_sections/HeroSection.tsx
@@ -20,12 +20,12 @@ function HeroSection() {
 
   return (
     <div className={styles.hero_section}>
-      <div className={`${styles.hero_section_content} ${styles.hero_image}`}>
-      </div>
+     {/*  <div className={`${styles.hero_section_content} ${styles.hero_image}`}>
+      </div> */}
       <div className={`${styles.hero_section_content}`}>
         <div className={styles.text_content}>
-          <h1>El futuro del bienestar en un Parche</h1>
-          <p>100% libre de drogas y no invasivo</p>
+          <h1>Regala Salud y Bienestar: <br/> Descubre los Packs Más Exclusivos para <br/><span> Navidad con 25% de Descuento. </span> </h1>
+          <p>El regalo perfecto que transformará vidas. Elige tu Pack diseñado para lograr más energía, más movilidad, más equilibrio y más belleza.</p>
           <div className={styles.button_container}>
             <PrimaryButton text="Cómo Funciona" onClick={navigateToVideoSection} />
           </div>
