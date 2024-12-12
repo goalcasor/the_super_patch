833c1ad44ae5724184030e620acdbaaf6a19865b
goalcasor <164497480+goalcasor@users.noreply.github.com>
2024-12-12 19:05:47 +0100

    Update WhatsAppButton.tsx

--- src/componets/buttons/WhatsAppButton.tsx
+++ src/componets/buttons/WhatsAppButton.tsx
@@ -11,9 +11,10 @@ function WhatsAppButton() {
   }
 
   return (
-    <a href="https://wa.me/34661959090" className={styles.wp_button}>
+    <a href="https://wa.me/34611825631?text=Hola%2C%20Quisiera%20más%20información%20sobre%20Super%20Patch" className={styles.wp_button}>
       <FaWhatsapp />
     </a>
+
   );
 }
 
