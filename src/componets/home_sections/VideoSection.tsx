dfc91a3a88127f83a0104174dcf6f809e657fc57
goalcasor <164497480+goalcasor@users.noreply.github.com>
2024-12-17 00:22:29 +0100

    Update VideoSection.tsx

--- src/componets/home_sections/VideoSection.tsx
+++ src/componets/home_sections/VideoSection.tsx
@@ -6,7 +6,7 @@ const VideoSection = () => {
        <div className={styles.video_section_container}>
         <div className={`${styles.video_section_content}`}>
           <div className={styles.video_section_text}>
-            <h1>Descrubre como un parche puede cambiar tu vida.</h1>
+            <h1>Descubre como un parche puede cambiar tu vida.</h1>
             <p>La tecnología dentro de Super Patch está compuesta por crestas dispuestas de forma innovadora, que recuerdan a un código QR. Al entrar en contacto con la piel, estas crestas envían señales únicas a nuestras células, que a su vez interactúan con el sistema nervioso, generando efectos específicos en el cuerpo.</p>
           </div>
 
